dd526d4fad7ba2a6183f254712d604f04f9ca22e
Harper <harper@barnett.work>
2021-03-26 15:03:05 +0000

    Creation of account and database information

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Post(models.Model):
+    body_text = models.CharField(max_length=240)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+class Vote(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE
+    )
+    hue = models.IntegerField(
+        validators=[MinValueValidator(1),MaxValueValidator(360)]
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
--- hueshift/settings.py
+++ hueshift/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
+    'feed.apps.FeedConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'account.User'

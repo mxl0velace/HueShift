7e746616eddb4994dcb53a36687804672d265d3d
Harper <harper@barnett.work>
2021-03-27 14:50:07 +0000

    Post menu changes

--- feed/forms.py
+++ feed/forms.py
@@ -6,3 +6,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['body_text']
+        labels = {
+            'body_text' : ''
+        }
+        widgets = {
+            'body_text' : forms.Textarea(attrs={
+                'rows' : 4,
+                'cols' : 80,
+                'maxlength' : Post._meta.get_field('body_text').max_length,
+                'style':'resize:none;',
+                'placeholder':'Piece text...'
+            })
+        }
